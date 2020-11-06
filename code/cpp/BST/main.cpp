636c466227dbf7fdedeee0dd5d88afa15be1cd39
Nicola Lambri <nicola.lambri@studenti.unimi.it>
2020-11-07 00:24:40 +0100

    Continue BST implementation

--- code/cpp/BST/main.cpp
+++ code/cpp/BST/main.cpp
@@ -16,7 +16,7 @@ BstNode<T> * GetNewNode (const T& v) {
 }
 
 template <class T>
-void insert (BstNode<T> *& pNode, const T& v) {
+void insert (BstNode<T> *& pNode, const T &v) {
     if (pNode == nullptr)
         pNode = GetNewNode (v);
     else if (v < pNode->value)
@@ -27,7 +27,7 @@ void insert (BstNode<T> *& pNode, const T& v) {
 }
 
 template <class T>
-int count_nodes (BstNode<T> *& pNode) {
+int count_nodes (BstNode<T> * const &pNode) {
     int count = 1;
     if (pNode == nullptr)
         return 0;
@@ -38,7 +38,7 @@ int count_nodes (BstNode<T> *& pNode) {
 
 // print values from min to max
 template <class T>
-void print (BstNode<T> *& pNode) {
+void print (BstNode<T> * const &pNode) {
     if (pNode == nullptr)
         return;
     print (pNode->left);
@@ -47,7 +47,7 @@ void print (BstNode<T> *& pNode) {
 }
 
 template <class T>
-void delete_tree (BstNode<T> *& pNode) {
+void delete_tree (BstNode<T> * &pNode) {
     if (pNode == nullptr)
         return;
     delete_tree (pNode->left);
@@ -56,7 +56,7 @@ void delete_tree (BstNode<T> *& pNode) {
 }
 
 template <class T>
-bool is_in_tree (BstNode<T> *& pNode, const T& v) {
+bool is_in_tree (BstNode<T> * const &pNode, const T &v) {
     if (pNode == nullptr)
         return false;
     else if (v < pNode->value)
@@ -67,22 +67,67 @@ bool is_in_tree (BstNode<T> *& pNode, const T& v) {
         
 }
 
+template <class T>
+T get_min (BstNode<T> * pNode) {
+    while (pNode->left != nullptr)
+        pNode = pNode->left;
+    return pNode->value;
+}
+
+template <class T>
+T get_max (BstNode<T> * pNode) {
+    while (pNode->right != nullptr)
+        pNode = pNode->right;
+    return pNode->value;
+}
+
+// return the next-highest value in tree after given value
+template <class T>
+T next_highest (BstNode<T> * pNode, T &v) {
+    T nexth;
+    while (pNode != nullptr) { // O(h)
+        if (v == pNode->value)
+            return v; // key = value
+        else if (v < pNode->value) {
+            nexth = pNode->value; // update nexth
+            pNode = pNode->left; // then go left
+        }
+        else {
+            pNode = pNode->right; // first move right
+            // we need the second condition to keep track of nexth
+            // if we go left then right
+            if (pNode != nullptr && pNode->value > nexth)
+                // then update nexth if nexth is smaller than next node's key
+                nexth = pNode->value;
+        }
+    }
+    if (v > nexth)
+        return -1; // value is greater than any key
+    else
+        return nexth;
+}
+
 int main () {
     BstNode<int> * root = nullptr;
+    int value;
     std::cout << "Number of nodes: " << count_nodes (root) << std::endl;
     // a recursive insert force to pass
     // BstNode * root as argument
-    insert (root, 10);
-    insert (root, 15);
-    insert (root, 4);
-    insert (root, 22);
-    insert (root, 14);
-    insert (root, 96);
-    insert (root, 46);
-    insert (root, 78);
+    insert (root, 50); insert (root, 34); insert (root, 61);
+    insert (root, 10); insert (root, 38); insert (root, 4);
+    insert (root, 22); insert (root, 55); insert (root, 96);
+    insert (root, 46); insert (root, 78);
     print (root);
     std::cout << "Number of nodes: " << count_nodes (root) << std::endl;
-    std::cout << "Is 46 in BST? " << is_in_tree (root, 46) << std::endl;
+    std::cout << "Insert value to search: "; std::cin >> value;
+    if (is_in_tree (root, value) == true)
+        std::cout << "Found." << std::endl;
+    else
+        std::cout << "Not found." << std::endl;
+    std::cout << "Min value in the tree: " << get_min (root) << std::endl;
+    std::cout << "Max value in the tree: " << get_max (root) << std::endl;
+    std::cout << "Insert value: "; std::cin >> value;
+    std::cout << "Next-highest to the value in the tree is: " << next_highest (root, value) << std::endl;
     delete_tree (root);
     std::cout << "Tree deleted." << std::endl;
 }
